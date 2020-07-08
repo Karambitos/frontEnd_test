28a4b1f36da3114cb64461444d9cbe2aa61951a7
KarambitoS <pase4nikdv@gmail.com>
2020-07-08 19:26:33 +0300

    cursor: pointer;

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 0,
     loop: true,
     pagination: {
       el: '.swiper-pagination',
