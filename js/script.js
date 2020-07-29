7321a24f44d7efe2de7c6de228b73c7cc6be6f8e
KarambitoS <pase4nikdv@gmail.com>
2020-07-29 12:15:07 +0300

    Update script.js
    Модуль 2 - задание 1-6

--- js/script.js
+++ js/script.js
@@ -1,29 +1,176 @@
 'use strict';
 
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
+   console.warn('---------модуль 2------------')
 
-  menuBtnRef.addEventListener('click', () => {
-    menuBtnRef.classList.toggle('is-open');
+// {
+//     console.warn('Задание 1')
+//     const logItems = function(array) {
+//       for (let i = 0; i < array.length; i++) {
+//         console.log([i+1] + ' - ' + array[i])
+//       }
+//     };
+//
+//     logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+//
+//     logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+// }
 
-    mobileMenuRef.classList.toggle('is-open');
-  });
-})();
+// {
+//   console.warn('Задание 2')
+//   const calculateEngravingPrice = function(message, pricePerWord) {
+//     let arr = message.split(' ');
+//     let total = arr.length * pricePerWord;
+//     console.log(total)
+//   };
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     10,
+//   );
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     20,
+//   );
+//   calculateEngravingPrice(
+//     'Donec orci lectus aliquam est magnis',
+//     40,
+//   );
+// }
 
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-nex',
-      prevEl: '.swiper-button-pre',
-    },
-  });
+// {
+//   console.warn('Задание 3')
+//   const findLongestWord = function(string) {
+//     let arr = string.split(' ');
+//     let longWord = arr[1];
+//     for (let i = 0; i < arr.length; i++) {
+//       let word = arr[i]; //3
+//       if (word.length > longWord.length){
+//         longWord = word;
+//         console.log('longWord', longWord);
+//       }
+//     }
+//   };
+//   findLongestWord('The quick brown fox jumped over the lazy dog');
+//   findLongestWord('Google do a roll');
+//   findLongestWord('May the force be with you');
+// }
+
+// {
+//   console.warn('Задание 4')
+//
+//   const formatString = function(string) {
+//     if (string.length > 40){
+//       let add = '...';
+//       let result = string.slice(0, 40);
+//       result = result.split(' ').concat(add);
+//       result = result.join(' ');
+//       string = result;
+//     }
+//     return string;
+//   };
+//
+//   /*
+//   /*
+//    * Вызовы функции для проверки работоспособности твоей реализации.
+//    */
+//   console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// // вернется оригинальная строка
+//
+//   console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// // вернется форматированная строка
+//
+//   console.log(formatString('Curabitur ligula sapien.'));
+// // вернется оригинальная строка
+//
+//   console.log(
+//     formatString(
+//       'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+//     ),
+//   );
+// // вернется форматированная строка
+// }
+
+// {
+//   console.warn('Задание 5')
+//
+//   const checkForSpam = function(message) {
+//     message = message.toLowerCase();
+//     message = message.includes('spam') || message.includes('sale');
+//     return message;
+//     // let result = false;
+//     // let lowerMessage = message.toLowerCase();
+//     // // let spam = lowerMessage.indexOf('spam');
+//     // // let sale = lowerMessage.indexOf('sale');
+//     // // if(spam !== -1 || sale !== -1) {
+//     // //   result = true;
+//     // // }
+//     //
+//     // let spam = 'spam';
+//     // let sale = 'sale';
+//     // if (lowerMessage.includes(spam) || lowerMessage.includes(sale)) {
+//     //   result = true;
+//     // }
+//     // return result;
+//   };
+//
+//   /*
+//    * Вызовы функции для проверки работоспособности твоей реализации.
+//    */
+//   console.log(checkForSpam('Latest technology news')); // false
+//
+//   console.log(checkForSpam('JavaScript weekly newsletter')); // false
+//
+//   console.log(checkForSpam('Get best sale offers now!')); // true
+//
+//   console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// }
+
+// {
+//   console.warn('Задание 6')
+//
+//   let input = prompt('Введите число');
+//   const numbers = [];
+//   let total = 0;
+//
+//     while (!(input === null)) {
+//         input = Number.parseInt(input);
+//         numbers.push(input);
+//         input = prompt('Введите число');
+//         }
+//     for (const number of numbers) {
+//       total += number ;
+//     }
+//   console.log(numbers)
+//   console.log(`Общая сумма чисел равна [${total}]`);
+// }
+
+{
+  console.warn('Задание 7')
+
+  const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+  const isLoginValid = function(login) {
+    // твой код
+  };
+
+  const isLoginUnique = function(allLogins, login) {
+    // твой код
+  };
+
+  const addLogin = function(allLogins, login) {
+    // твой код
+  };
+
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+  console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+  console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+  console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+}
+
+
+   console.warn('---------модуль 1------------')
 
 // {
 //     console.warn('Задание 1')
@@ -36,9 +183,9 @@ var swiper = new Swiper('.swiper-container', {
 
 // {
 //     console.warn('Задание 2')
+//
 //     const total = 100;
 //     let ordered = prompt('Какое количества товаров Вам необходимо?');
-    
 //     if(ordered === null){
 //         console.log("Отменено пользователем!");
 //         } else if(Number.parseInt(ordered) <= total){
@@ -52,6 +199,7 @@ var swiper = new Swiper('.swiper-container', {
 
 // {
 //     console.warn('Задание 3')
+//
 //     const ADMIN_PASSWORD = 'jqueryismyjam';
 //     let message = prompt('Введите ваш пароль');
 //     switch(message) {
@@ -61,7 +209,7 @@ var swiper = new Swiper('.swiper-container', {
 //         case '':
 //             console.log("Вы ни чего не ввели!");
 //             break;
-//         case ADMIN_PASSWORD: 
+//         case ADMIN_PASSWORD:
 //             console.log("Добро пожаловать!");
 //             break;
 //         default:
@@ -70,18 +218,9 @@ var swiper = new Swiper('.swiper-container', {
 //     alert(message);
 // }
 
-// if(ordered === null){
-//     console.log("Отменено пользователем!");
-//     } else if(typeof ordered === "string"){
-//         console.log("Введите число!");
-//     }else if(Number.parseInt(ordered) <= total){
-//         console.log("Заказ оформлен, с вами свяжется менеджер");
-//     } else {
-//         console.log("На складе недостаточно твоаров!");
-//     }
-
 // {
 //     console.warn('Задание 4')
+//
 //     let credits = 23580;
 //     const pricePerDroid = 3000;
 //     let droidOrder = prompt('Какое количество дроидов Вам необходимо?');
@@ -97,12 +236,13 @@ var swiper = new Swiper('.swiper-container', {
 //         }
 //       } else {
 //         console.log(`Вы ввели не числовое значение!`)
-
+//
 //       }
 // }
 
 // {
 //     console.warn('Задание 5')
+//
 //     let countryImput = (prompt('В какую страну Вы хотите оформить доставку товара?')).toLowerCase();
 //     let countryDelivery = countryImput[0].toUpperCase().concat(countryImput.slice(1, ));
 //     switch(countryDelivery) {
@@ -112,16 +252,16 @@ var swiper = new Swiper('.swiper-container', {
 //         case 'Чили':
 //             console.log(`Доставка в [${countryDelivery}] будет стоить [250] кредитов`);
 //             break;
-//         case 'Австралия': 
+//         case 'Австралия':
 //             console.log(`Доставка в [${countryDelivery}] будет стоить [170] кредитов`);
 //             break;
-//         case 'Индия': 
+//         case 'Индия':
 //             console.log(`Доставка в [${countryDelivery}] будет стоить [80] кредитов`);
 //             break;
-//         case 'Ямайка': 
+//         case 'Ямайка':
 //             console.log(`Доставка в [${countryDelivery}] будет стоить [120] кредитов`);
 //             break;
-//         case 'Украина': 
+//         case 'Украина':
 //             console.log(`Доставка в [${countryDelivery}] будет стоить [100500] кредитов + страховка УкрПОЧТЫ [999 000 грн]`);
 //             break;
 //         default:
@@ -133,15 +273,13 @@ var swiper = new Swiper('.swiper-container', {
 
 // {
 //     console.warn('Задание 6')
-
+//
 //     let input = 0;
 //     let total = 0;
-
-//     input = prompt('Ввведите число');
+//     input = prompt('Введите число');
 //     total += Number(input);
-
 //     while (!(input === null)) {
-//         input = prompt('Ввведите число');
+//         input = prompt('Введите число');
 //         total += Number(input);
 //         console.log(total);
 //     }
